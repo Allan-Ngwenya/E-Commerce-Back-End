62d62375886a9b10db9ca86474626556fbbfd4b4
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2022-01-30 20:25:16 +0000

    category routes for delete

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -74,3 +74,26 @@ router.put('/:id', (req, res) => {
 		});
 
 });
+
+router.delete('/:id', (req, res) => {
+	// delete a category
+	Category.destroy({
+		where: {
+			id: req.params.id
+		}
+	})
+		.then(dbCategoryData => {
+			if (!dbCategoryData) {
+				res.status(404).json({ message: 'No category found with this id' });
+				return;
+			}
+			res.json(dbCategoryData);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+
+module.exports = router;
